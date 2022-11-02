ee8e0b70e7f48043986c91945f1fb443011386d5
Teemu Viitanen <teemu.viitanen@gmail.com>
2022-11-02 23:22:12 +0200

    initial commit

--- node_crash_course/index.js
+++ node_crash_course/index.js
@@ -86,6 +86,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
